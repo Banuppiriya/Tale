0c4c254f5126842c36271798ca461183e744d610
Your Name <theepika0028@gmail.com>
2025-04-22 16:32:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+//import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Section from './components/Section';
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <Header /> 
+      <Section/>
+      <Footer/>
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <footer>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <p>Copyright © 2023 Company Name</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+export default Footer;
+
